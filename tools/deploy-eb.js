31eda7d04a77094cc4e93356c8c7e8251f26c680
Justin Worsdale <whayler1@gmail.com>
2017-09-04 21:28:43 -0400

    add eb deploy

--- tools/deploy-eb.js
+++ tools/deploy-eb.js
@@ -10,52 +10,11 @@ const options = {
 };
 
 /**
- * Deploy the contents of the `/build` folder to a remote server via Git.
+ * Deploy the contents of the `/build` folder to elastic beanstalk.
  */
 async function deploy() {
   // Initialize a new repository
   await makeDir('build');
-  // await spawn('git', ['init', '--quiet'], options);
-
-  // Changing a remote's URL
-  // let isRemoteExists = false;
-  // try {
-  //   await spawn(
-  //     'git',
-  //     ['config', '--get', `remote.${remote.name}.url`],
-  //     options,
-  //   );
-  //   isRemoteExists = true;
-  // } catch (error) {
-  //   /* skip */
-  // }
-  // await spawn(
-  //   'git',
-  //   ['remote', isRemoteExists ? 'set-url' : 'add', remote.name, remote.url],
-  //   options,
-  // );
-
-  // Fetch the remote repository if it exists
-  // let isRefExists = false;
-  // try {
-  //   await spawn(
-  //     'git',
-  //     ['ls-remote', '--quiet', '--exit-code', remote.url, remote.branch],
-  //     options,
-  //   );
-  //   isRefExists = true;
-  // } catch (error) {
-  //   await spawn('git', ['update-ref', '-d', 'HEAD'], options);
-  // }
-  // if (isRefExists) {
-  //   await spawn('git', ['fetch', remote.name], options);
-  //   await spawn(
-  //     'git',
-  //     ['reset', `${remote.name}/${remote.branch}`, '--hard'],
-  //     options,
-  //   );
-  //   await spawn('git', ['clean', '--force'], options);
-  // }
 
   // Build the project in RELEASE mode which
   // generates optimized and minimized bundles
@@ -71,33 +30,13 @@ async function deploy() {
     await moveDir('build/public', 'build');
   }
 
-  // zip -9 -r build.zip *
   // Zip the contents of the build folder
-  // console.log('process.argv', process.argv);
   await spawn('zip', ['-9', '-r', 'build.zip', '.'], options);
 
-  // Push the contents of the build folder to the remote server via Git
-  // await spawn('git', ['add', '.', '--all'], options);
-  // try {
-  //   await spawn('git', ['diff', '--cached', '--exit-code', '--quiet'], options);
-  // } catch (error) {
-  //   await spawn(
-  //     'git',
-  //     ['commit', '--message', `Update ${new Date().toISOString()}`],
-  //     options,
-  //   );
-  // }
-  // await spawn(
-  //   'git',
-  //   ['push', remote.name, `master:${remote.branch}`, '--set-upstream'],
-  //   options,
-  // );
-
-  // Check if the site was successfully deployed
-  // const response = await fetch(remote.website);
-  // console.info(
-  //   `${remote.website} => ${response.status} ${response.statusText}`,
-  // );
+  // Deploy the zip to eb
+  await spawn('eb', ['deploy', '--staged'], {
+    stdio: ['ignore', 'inherit', 'inherit'],
+  });
 }
 
 export default deploy;
