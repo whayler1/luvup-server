252618a49c51ede43c7d88e1c57159d0a8f1ac2d
Justin Worsdale <whayler1@gmail.com>
2017-09-09 20:16:35 -0400

    create dict from query args

--- src/data/queries/listings.js
+++ src/data/queries/listings.js
@@ -2,6 +2,20 @@ import { GraphQLInt, GraphQLObjectType, GraphQLList as List } from 'graphql';
 import ListingType from '../types/ListingType';
 import { Listing } from '../models';
 
+const selectionToDict = selections =>
+  selections.reduce(
+    (obj, selection) => ({
+      ...obj,
+      [selection.name.value]: selection.selectionSet
+        ? selectionToDict(selection.selectionSet.selections)
+        : null,
+    }),
+    {},
+  );
+
+const getFieldObj = ast =>
+  selectionToDict(ast.fieldNodes[0].selectionSet.selections);
+
 const listings = {
   type: new GraphQLObjectType({
     name: 'ListingResource',
@@ -14,14 +28,17 @@ const listings = {
     limit: { type: GraphQLInt },
     offset: { type: GraphQLInt },
   },
-  resolve: async ({ request }, { limit, offset }) => {
+  resolve: async ({ request }, { limit, offset }, root, ast) => {
     const res = await Listing.findAndCountAll({ limit, offset });
-    const promises = res.rows.map(listing => {
-      const promise = listing.getLocation();
-      promise.then(location => Object.assign(listing, { location }));
-      return promise;
-    });
-    await Promise.all(promises);
+    const fields = getFieldObj(ast);
+    if ('rows' in fields && 'location' in fields.rows) {
+      const promises = res.rows.map(listing => {
+        const promise = listing.getLocation();
+        promise.then(location => Object.assign(listing, { location }));
+        return promise;
+      });
+      await Promise.all(promises);
+    }
 
     return res;
   },
