3116a106bb4af9618544814c3801f1b03accaf4b
Justin Worsdale <whayler1@gmail.com>
2017-11-16 22:06:55 -0500

    jwt verify stores entire user

--- src/data/queries/me.js
+++ src/data/queries/me.js
@@ -1,16 +1,41 @@
+import jwt from 'jsonwebtoken';
 import { User, UserLocal } from '../models';
 
 import UserType from '../types/UserType';
+import config from '../../config';
 
 const me = {
   type: UserType,
-  resolve({ request }) {
-    // console.log('request', request);
+  resolve: async ({ request }) => {
+    console.log('request', request);
+    const verify = await new Promise(resolve =>
+      jwt.verify(
+        request.cookies.id_token,
+        config.auth.jwt.secret,
+        (err, result) => {
+          if (err) {
+            // console.log('\n\n error', err);
+            resolve({
+              ok: false,
+              result: err,
+            });
+          } else {
+            // console.log('\n\n result', result);
+            resolve({
+              ok: true,
+              result,
+            });
+          }
+        },
+      ),
+    );
+
+    console.log('\n\nverify', verify);
+
     return (
       request.user && {
-        id: request.user.id,
-        email: request.user.email,
-        profile: { a: 'a' },
+        id: verify.result.id,
+        email: verify.result.email,
       }
     );
   },
