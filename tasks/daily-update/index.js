28807b8844ea7e8e9c3985f120a46718805b5e91
Justin Worsdale <whayler1@gmail.com>
2018-05-21 22:26:33 -0400

    send simple message to all valid tokens

--- tasks/daily-update/index.js
+++ tasks/daily-update/index.js
@@ -1,6 +1,15 @@
 const { Pool } = require('pg');
+const Expo = require('expo-server-sdk');
 
 const connectionString = process.env.DATABASE_URL;
+
+const expo = new Expo();
+
+const getFilteredTokens = tokens =>
+  tokens.filter(token => {
+    const isValid = Expo.isExpoPushToken(token.token);
+    return isValid;
+  });
 
 const getValidTokens = async () => {
   const pool = await new Pool({ connectionString });
@@ -12,8 +21,40 @@ const getValidTokens = async () => {
   return rows;
 };
 
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
+const sendChunks = async chunks => {
+  const erroredTokens = [];
+  for (const chunk of chunks) {
+    try {
+      await expo.sendPushNotificationAsync(chunk);
+    } catch (error) {
+      console.error('error sending push tokens', error);
+      erroredTokens.push(chunk.to);
+    }
+  }
+
+  return { erroredTokens };
+};
+/* eslint-enable no-restricted-syntax */
+/* eslint-enable no-await-in-loop */
+
 exports.handler = async () => {
   const validTokens = await getValidTokens();
+  const filteredTokens = getFilteredTokens(validTokens);
+
+  const notifications = filteredTokens.map(token => ({
+    to: token.token,
+    body: 'Let your lover know you care! Send a love up.',
+    data: {
+      type: 'daily-update',
+    },
+    sound: 'default',
+  }));
+  const chunks = expo.chunkPushNotifications(notifications);
+
+  await sendChunks(notifications);
+
   console.log('validTokens', validTokens);
 
   return 'daily update done';
