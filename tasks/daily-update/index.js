58c7fee3479c1ccbadf3b15ea4f478d3a8c1e4d8
Justin Worsdale <whayler1@gmail.com>
2018-05-21 22:03:05 -0400

    lambda pg connection

--- tasks/daily-update/index.js
+++ tasks/daily-update/index.js
@@ -1,1 +1,22 @@
-console.log('daily update ran');
+const { Pool } = require('pg');
+
+const connectionString = process.env.DATABASE_URL;
+
+exports.handler = async () => {
+  const pool = await new Pool({
+    connectionString,
+  });
+
+  await new Promise(resolve =>
+    pool.query(
+      'select * from public."ExpoPushToken" where "isValid" = true',
+      (err, res) => {
+        console.log(err, res);
+        pool.end();
+        resolve();
+      },
+    ),
+  );
+
+  return 'daily update done';
+};
