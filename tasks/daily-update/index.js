c91438ba98f08b05b1459c0f74bce0d9bc5e8ee4
Justin Worsdale <whayler1@gmail.com>
2018-06-03 21:29:29 -0400

    get activity message promise array

--- tasks/daily-update/index.js
+++ tasks/daily-update/index.js
@@ -1,9 +1,12 @@
 const { Pool } = require('pg');
 const Expo = require('expo-server-sdk');
+const moment = require('moment');
 
 const connectionString = process.env.DATABASE_URL;
 
 const expo = new Expo();
+const now = moment();
+const yesterday = moment().subtract(1, 'd');
 
 const getFilteredTokens = tokens =>
   tokens.filter(token => {
@@ -11,14 +14,44 @@ const getFilteredTokens = tokens =>
     return isValid;
   });
 
-const getValidTokens = async () => {
-  const pool = await new Pool({ connectionString });
+const getValidTokens = async pool => {
   const { rows } = await pool.query(
-    'select token, "fullName" from public."ExpoPushToken" t, public."User" u where t."userId" = u.id and t."isValid" = true',
+    'select token, "fullName", "userId", "RelationshipId" from public."ExpoPushToken" t, public."User" u where t."userId" = u.id and t."isValid" = true',
   );
-  pool.end();
 
   return rows;
+};
+
+const getLover = async (pool, userId, relationshipId) => {
+  const { rows } = await pool.query(
+    `select * from public."User" u where u."RelationshipId" = '${relationshipId}' and u.id != '${userId}';`,
+  );
+  return rows[0];
+};
+
+const getActivityMessage = async (pool, token) => {
+  console.log('getActivityMessage');
+  const { userId, RelationshipId } = token;
+  const lover = await getLover(pool, userId, RelationshipId);
+
+  const jalapenoCount = await pool.query(
+    `select count(*) from public."Jalapeno" j where j."recipientId" = '${userId}' and j."createdAt" > '${yesterday.format(
+      'YYYY-MM-DD',
+    )}';`,
+  );
+  const luvupCount = await pool.query(
+    `select count(*) from public."Coin" l where l."recipientId" = '${userId}' and l."createdAt" > '${yesterday.format(
+      'YYYY-MM-DD',
+    )}';`,
+  );
+
+  // console.log('\n\ntoken\n', token);
+  // console.log('\n\nluvupCount', luvupCount, '\njalapenoCount', jalapenoCount);
+  // get luvups/jalapenos for last day
+  // if no luvups or jalapenos
+  // if jalapenos
+  // if luvups
+  return { jalapenoCount, luvupCount };
 };
 
 /* eslint-disable no-restricted-syntax */
@@ -39,32 +72,71 @@ const sendChunks = async chunks => {
 /* eslint-enable no-restricted-syntax */
 /* eslint-enable no-await-in-loop */
 
-const messages = [
-  "Weird vibes got you down? Maybe it's time to send someone a Luvup and let them know you care.",
-  'They say only sending Luvups can mend a broken heart.',
-  "Maybe if you had sent your lover more Luvups you wouldn't be in this situation in the first place.",
-  'Let your lover know you care! Send a Luvup.',
-  "A Luvup a day keeps the sickening sadness of a lifetime of leneliness away… Just sayin'",
-];
-
-const getMessage = () => messages[Math.floor(Math.random() * messages.length)];
+// const messages = [
+//   "Weird vibes got you down? Maybe it's time to send someone a Luvup and let them know you care.",
+//   'They say only sending Luvups can mend a broken heart.',
+//   "Maybe if you had sent your lover more Luvups you wouldn't be in this situation in the first place.",
+//   'Let your lover know you care! Send a Luvup.',
+//   "A Luvup a day keeps the sickening sadness of a lifetime of leneliness away… Just sayin'",
+// ];
+//
+// const getMessage = () => messages[Math.floor(Math.random() * messages.length)];
 
 exports.handler = async () => {
-  const validTokens = await getValidTokens();
+  const pool = await new Pool({ connectionString });
+  const validTokens = await getValidTokens(pool);
   const filteredTokens = getFilteredTokens(validTokens);
-  const body = getMessage();
+  console.log('filteredTokens', filteredTokens);
 
-  const notifications = filteredTokens.map(token => ({
-    to: token.token,
-    body,
-    data: {
-      type: 'daily-update',
-    },
-    sound: 'default',
-  }));
-  const chunks = expo.chunkPushNotifications(notifications);
+  const promises = filteredTokens.map(
+    token =>
+      new Promise(resolve => {
+        (async () => {
+          console.log('async func');
+          await getActivityMessage(pool, token);
+          resolve();
+        })();
+      }),
+  );
 
-  await sendChunks(notifications);
+  console.log('promises', promises);
+  // const promises = filteredTokens.map(token => new Promise((resolve) => async () => {
+  //     console.log('inside promise');
+  //     await getActivityMessage(pool, token);
+  //     resolve();
+  //   }
+  // ));
+  // console.log('promises', promises);
+  //
+  const res = await Promise.all(promises);
+
+  console.log('after promise all', res);
+  // const notifications = filteredTokens.map(token => ({
+  //   to: token.token,
+  //   body,
+  //   data: {
+  //     type: 'daily-update',
+  //   },
+  //   sound: 'default',
+  // }));
+  // console.log({ notifications });
+  // const chunks = expo.chunkPushNotifications(notifications);
+
+  // // await sendChunks(chunks);
+  // const promises = filteredTokens.map(token =>
+  //   new Promise(resolve => {
+  //     sendContextualMessagesFarm(
+  //       { token: token.token },
+  //       (err, output) => resolve()
+  //     );
+  //   }));
+
+  // console.log('\npromises', promises);
+
+  // await Promise.all(promises);
+  // workerFarm.end(sendContextualMessagesFarm);
+
+  pool.end();
 
   return 'daily update done';
 };
