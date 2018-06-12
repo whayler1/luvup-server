6e1a134474f2eec7dd7e230bb25c6e61550e8c71
Justin Worsdale <whayler1@gmail.com>
2018-06-11 20:47:16 -0400

    set relationship scores syncronously in set relationship score task

--- tasks/set-relationship-score/index.js
+++ tasks/set-relationship-score/index.js
@@ -1,12 +1,20 @@
+const { Pool } = require('pg');
 const superagent = require('superagent');
 const _ = require('lodash');
 
 const ROOT_URL = process.env.ROOT_URL;
 const ADMIN_TOKEN = process.env.ADMIN_TOKEN;
+const connectionString = process.env.DATABASE_URL;
+
+const getUsers = async pool => {
+  const { rows } = await pool.query('select id from public."User";');
+
+  return rows;
+};
 
 const updateRelationshipScore = async userId => {
   try {
-    const res = superagent(`${ROOT_URL}/graphql`, {
+    const res = await superagent.post(`${ROOT_URL}/graphql`, {
       query: `mutation {
         createRelationshipScore(
           userId: "${userId}",
@@ -33,7 +41,26 @@ const updateRelationshipScore = async userId => {
   }
 };
 
-exports.handler = async userId => {
-  const res = await updateRelationshipScore(userId);
-  return res;
+exports.handler = async () => {
+  const pool = await new Pool({ connectionString });
+  const users = await getUsers(pool);
+
+  await users.reduce(
+    (p, user) =>
+      p
+        .then(async () => {
+          try {
+            const res = await updateRelationshipScore(user.id);
+            return res;
+          } catch (err) {
+            throw new Error(err);
+          }
+        })
+        .catch(err => console.log({ err })),
+    Promise.resolve(),
+  );
+
+  pool.end();
+
+  return 'omg';
 };
