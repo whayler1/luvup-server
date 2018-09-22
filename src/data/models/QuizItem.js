4e3f72c0398e7b7705336d8a0ab1353d0da422f8
Justin Worsdale <whayler1@gmail.com>
2018-09-22 13:45:28 -0400

    add isArchived bool to QuizItem model

--- src/data/models/QuizItem.js
+++ src/data/models/QuizItem.js
@@ -11,6 +11,14 @@ const QuizItem = Model.define('QuizItem', {
   question: { type: DataType.TEXT },
   senderChoiceId: { type: DataType.UUID },
   recipientChoiceId: { type: DataType.UUID },
+  reward: {
+    type: DataType.INTEGER,
+    min: 0,
+  },
+  isArchived: {
+    type: DataType.BOOLEAN,
+    defaultValue: false,
+  },
 });
 
 export default QuizItem;
