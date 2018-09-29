04a5a635885106762fa79782db3b4c4d198392f6
Justin Worsdale <whayler1@gmail.com>
2018-09-29 15:09:03 -0500

    createQuizItem required args error test

--- src/data/mutations/createQuizItem.test.js
+++ src/data/mutations/createQuizItem.test.js
@@ -2,8 +2,8 @@ import { graphql } from 'graphql';
 import schema from '../schema';
 
 describe('createQuizItem', () => {
-  describe('when question arg not provided', () => {
-    it('should throw an error', async () => {
+  describe('when required args not provided', () => {
+    it('should return required args console.error(); array', async () => {
       const query = `mutation {
         createQuizItem {
           quizItem { id }
@@ -11,7 +11,36 @@ describe('createQuizItem', () => {
       }`;
 
       const result = await graphql(schema, query, {}, {});
-      console.log('result', result);
+
+      expect(result.errors).toHaveLength(4);
+      expect(result.errors).toEqual(
+        expect.arrayContaining([
+          {
+            message:
+              'Field "createQuizItem" argument "question" of type "String!" is required but not provided.',
+            locations: [{ line: 2, column: 9 }],
+            path: undefined,
+          },
+          {
+            message:
+              'Field "createQuizItem" argument "reward" of type "Int!" is required but not provided.',
+            locations: [{ line: 2, column: 9 }],
+            path: undefined,
+          },
+          {
+            message:
+              'Field "createQuizItem" argument "choices" of type "[String]!" is required but not provided.',
+            locations: [{ line: 2, column: 9 }],
+            path: undefined,
+          },
+          {
+            message:
+              'Field "createQuizItem" argument "senderChoiceId" of type "ID!" is required but not provided.',
+            locations: [{ line: 2, column: 9 }],
+            path: undefined,
+          },
+        ]),
+      );
     });
   });
   xit('should respond correctly when user not logged in', () => {
