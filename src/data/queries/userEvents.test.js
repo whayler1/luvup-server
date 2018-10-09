daa65c3300651316b6543f7ab61a88facec37170
Justin Worsdale <whayler1@gmail.com>
2018-10-08 20:08:42 -0400

    test should offset properly

--- src/data/queries/userEvents.test.js
+++ src/data/queries/userEvents.test.js
@@ -4,36 +4,39 @@ import schema from '../schema';
 import { createLoggedInUser } from '../test-helpers';
 import { UserEvent } from '../models';
 
+const getSuccessfulQuery = async offset => {
+  const { user, rootValue } = await createLoggedInUser();
+  const eventNames = ['coin-received', 'lovenote-sent', 'quiz-item-sent'];
+  await UserEvent.bulkCreate(
+    eventNames.map((name, i) => ({
+      userId: user.id,
+      relationshipId: user.RelationshipId,
+      createdAt: new Date(`2018-01-0${i + 1}`),
+      updatedAt: new Date(`2018-01-0${i + 1}`),
+      name,
+    })),
+  );
+  const offsetStr = offset ? `offset: ${offset}` : '';
+  const query = `{
+    userEvents(
+      limit: 2 ${offsetStr}
+    ) {
+      rows { name }
+      count
+      limit
+      offset
+    }
+  }`;
+  const res = await graphql(schema, query, rootValue, sequelize);
+  return { user, res };
+};
+
 describe('userEvents', () => {
   describe('when user is logged in', () => {
-    it('should return user events', async () => {
-      const { user, rootValue } = await createLoggedInUser();
-      const eventNames = ['coin-received', 'lovenote-sent', 'quiz-item-sent'];
-      await UserEvent.bulkCreate(
-        eventNames.map((name, i) => ({
-          userId: user.id,
-          relationshipId: user.RelationshipId,
-          createdAt: new Date(`2018-01-0${i + 1}`),
-          updatedAt: new Date(`2018-01-0${i + 1}`),
-          name,
-        })),
-      );
-      const query = `{
-        userEvents(
-          limit: 2
-        ) {
-          rows { name }
-          count
-          limit
-          offset
-        }
-      }`;
-      const { data: { userEvents: { rows, count } } } = await graphql(
-        schema,
-        query,
-        rootValue,
-        sequelize,
-      );
+    it('should return user events starting with most recent', async () => {
+      const {
+        res: { data: { userEvents: { rows, count, limit, offset } } },
+      } = await getSuccessfulQuery();
 
       expect(rows).toHaveLength(2);
       expect(count).toEqual(3);
@@ -45,6 +48,27 @@ describe('userEvents', () => {
       expect(rows[1]).toEqual(
         expect.objectContaining({
           name: 'lovenote-sent',
+        }),
+      );
+      expect(limit).toEqual(2);
+      expect(offset).toBeNull();
+    });
+
+    it('should offset properly', async () => {
+      const {
+        res: { data: { userEvents: { rows, count } } },
+      } = await getSuccessfulQuery(1);
+
+      expect(rows).toHaveLength(2);
+      expect(count).toEqual(3);
+      expect(rows[0]).toEqual(
+        expect.objectContaining({
+          name: 'lovenote-sent',
+        }),
+      );
+      expect(rows[1]).toEqual(
+        expect.objectContaining({
+          name: 'coin-received',
         }),
       );
     });
