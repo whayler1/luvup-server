c917e5d789d84f95cd48262a945bfa5965c9f95b
Justin Worsdale <whayler1@gmail.com>
2018-10-09 20:46:55 -0400

    edit jasmine timeout in createQuizItem

--- src/data/mutations/createQuizItem.test.js
+++ src/data/mutations/createQuizItem.test.js
@@ -40,8 +40,16 @@ const getSuccessfulCreateQuizItemResponse = async () => {
 };
 
 describe('createQuizItem', () => {
+  let originalTimeout;
+
   beforeAll(async () => {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
     await modelsSync;
+  });
+
+  afterAll(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
   describe('when required args not provided', () => {
