58e6e0c4327df2167ee93992de70d70f1e90e579
Justin Worsdale <whayler1@gmail.com>
2018-10-23 20:34:46 -0400

    test for required args

--- src/data/mutations/setLoveNoteRead.test.js
+++ src/data/mutations/setLoveNoteRead.test.js
@@ -18,6 +18,26 @@ describe('setLoveNoteRead', () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
+  describe('when loveNoteId arg not provided', () => {
+    it('should return required args error', async () => {
+      const query = `mutation {
+        setLoveNoteRead {
+          loveNote { id }
+        }
+      }`;
+
+      const result = await graphql(schema, query, {}, sequelize);
+
+      expect(result.errors).toHaveLength(1);
+      expect(result.errors[0]).toEqual(
+        expect.objectContaining({
+          message:
+            'Field "setLoveNoteRead" argument "loveNoteId" of type "ID!" is required but not provided.',
+        }),
+      );
+    });
+  });
+
   describe('when user note logged in', () => {
     it('should return an error', async () => {
       const query = `mutation {
