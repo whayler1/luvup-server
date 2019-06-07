5f949e80baf4fc11850f69e1acc8eb1775342e1a
Justin Worsdale <whayler1@gmail.com>
2019-06-06 22:59:26 -0400

    move acceptLoverRequest tracking to its own file

--- src/data/mutations/acceptLoverRequest.js
+++ src/data/mutations/acceptLoverRequest.js
@@ -1,34 +1,14 @@
 import { GraphQLString, GraphQLObjectType } from 'graphql';
-// import moment from 'moment';
 
 import LoverRequestType from '../types/LoverRequestType';
 import RelationshipType from '../types/RelationshipType';
 import { acceptLoverRequest as sendPushNotification } from '../../services/pushNotifications';
-// import { User, LoverRequest, Relationship } from '../models';
-// import { datetimeAndTimestamp } from '../helpers/dateFormats';
+import { sendAcceptLoverRequestEmails } from '../../emails';
 import {
-  emailHelper,
   acceptLoverRequestAndDuplicatePlaceholderDataForLover,
   validateJwtToken,
 } from '../helpers';
-import analytics from '../../services/analytics';
-// import { sendPushNotification } from '../../services/pushNotifications';
-// import { LoverRequestNotFoundError } from '../errors';
-
-const sendEmails = (sender, recipient) => {
-  const senderEmail = emailHelper.sendEmail({
-    to: sender.email,
-    subject: 'You have been accepted by a new lover!',
-    html: `<p>Hi ${sender.fullName},</p><p>Congratulations, <b>${recipient.fullName}</b> has accepted your lover request on Luvup!</p>`,
-  });
-  const recipientEmail = emailHelper.sendEmail({
-    to: recipient.email,
-    subject: 'You have accepted a new lover!',
-    html: `<p>Hi ${recipient.fullName},</p><p>Congratulations, you have accepted <b>${sender.fullName}</b> as your new lover on Luvup!</p>`,
-  });
-
-  return Promise.all([senderEmail, recipientEmail]);
-};
+import { trackAcceptLoverRequest } from '../../services/analytics';
 
 const acceptLoverRequest = {
   type: new GraphQLObjectType({
@@ -48,7 +28,6 @@ const acceptLoverRequest = {
     const {
       user,
       sender,
-      // placeholderLover,
       loverRequest,
       relationship,
     } = await acceptLoverRequestAndDuplicatePlaceholderDataForLover(
@@ -56,23 +35,10 @@ const acceptLoverRequest = {
       loverRequestId,
     );
 
-    analytics.track({
-      userId: user.id,
-      event: 'acceptLoverRequest',
-      properties: {
-        category: 'loverRequest',
-        loverRequestId,
-        senderId: sender.id,
-      },
-    });
-
+    trackAcceptLoverRequest(user.id, sender.id, loverRequest.id);
     sendPushNotification(sender, user);
+    sendAcceptLoverRequestEmails(sender, user);
 
-    try {
-      await sendEmails(sender, user);
-    } catch (err) {
-      console.error('error sending acceptLoverRequest email');
-    }
     return {
       loverRequest,
       relationship: {
