64081b437c95e4db27bf92b63f6dd5adf94f882f
Justin Worsdale <whayler1@gmail.com>
2019-06-24 23:29:30 -0400

    create relationshipInitializer

---
+++ src/data/helpers/relationshipInitializer.js
@@ -0,0 +1,14 @@
+import {
+  Relationship,
+  RelationshipInvite,
+  User,
+  LoverRequest,
+} from '../models';
+
+export const createRelationshipAndPlaceholderLoverWithExistingRecipient = recipientId => {};
+
+export const createRelationshipAndPlaceholderLoverWithLoverRequestInvite = (
+  recipientFirstName,
+  recipientLastName,
+  recipientEmail,
+) => {};
